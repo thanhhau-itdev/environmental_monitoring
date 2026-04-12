1f3dc8c34a3e1e30e4908881dcef5e12b276fd18
Danh Thanh Hậu <129538148+thanhhau-itdev@users.noreply.github.com>
2026-04-12 19:34:05 +0700

    Thanh hau (#17)
    * u project
    
    create file readme, schematic esp32c5 pins
    
    * u file readme
    
    * u readme
    
    * change image file
    
    * change folder
    
    * u project
    
    create library relay
    u file main
    
    * u project
    
    main1 controller
    main2 controller
    libraries
    
    * u project
    
    u all
    
    * u main
    
    * u OTA main2 & lib ArduinoJson
    
    * u firmware & OTA
    
    * remove lib and folder
    
    * u OTA, u all
    
    * u OTA
    
    * u OTA main all
    
    * u double main, u OTA
    
    * u instruct_web

--- instruct_web/uploads/upload_firmware.php
+++ instruct_web/uploads/upload_firmware.php
@@ -5,34 +5,27 @@ $conn = new mysqli("localhost", "root", "", "htqt");
 $fw1 = $_POST["firmware_board_read_sensor"];
 $fw2 = $_POST["firmware_board_upload_data"];
 
-$sql_1 = "SELECT name, status FROM upload_firmwares WHERE firmware = '$fw1'";
-$sql_2 = "SELECT name, status FROM upload_firmwares WHERE firmware = '$fw2'";
-
-$result_1 = $conn->query($sql_1)->fetch_assoc();
-$result_2 = $conn->query($sql_2)->fetch_assoc();
-
+$sql = "SELECT firmware, name, status FROM upload_firmwares WHERE firmware IN ('$fw1','$fw2')";
+$result = $conn->query($sql);
 $response = [];
 
-if ($result_1 && $result_1['status'] == 1) {
+while ($row = $result->fetch_assoc()) {
+    if ($row['status'] == 1) {
 
-    $response[] = [
-        'firmware' => $fw1,
-        'name' => $result_1['name'],
-        'file' => "http://192.168.1.111/htqt/files/" . $result_1['name']
-    ];
+        if ($row['firmware'] == $fw1) {
+            $file = "http://192.168.1.111/htqt/files/read_sensors.ino.bin";
+        } else {
+            $file = "http://192.168.1.111/htqt/files/upload_datas.ino.bin";
+        }
 
-    $conn->query("UPDATE upload_firmwares SET status = 0 WHERE firmware = '$fw1'");
-}
+        $response[] = [
+            'firmware' => $row['firmware'],
+            'name' => $row['name'],
+            'file' => $file
+        ];
 
-if ($result_2 && $result_2['status'] == 1) {
-
-    $response[] = [
-        'firmware' => $fw2,
-        'name' => $result_2['name'],
-        'file' => "http://192.168.1.111/htqt/files/" . $result_2['name']
-    ];
-
-    $conn->query("UPDATE upload_firmwares SET status = 0 WHERE firmware = '$fw2'");
+        $conn->query("UPDATE upload_firmwares SET status = 0 WHERE firmware = '".$row['firmware']."'");
+    }
 }
 
 if (empty($response)) {
